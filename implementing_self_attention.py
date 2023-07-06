772f378045a334c0812880c90359b7811a5d4f9b
Jaward Sesay <jkef96@icloud.com>
2023-07-06 10:02:25 +0800

    Implementing simple transformer self-attention block

--- implementing_self_attention.py
+++ implementing_self_attention.py
@@ -1,4 +1,4 @@
-# Implementing tranformer self-attention block
+# Implementing simple tranformer self-attention block
 torch.manual_seed(1337)
 B,T,C = 4,8,32 # batch, time, channels
 x = torch.randn(B,T,C)
@@ -13,7 +13,7 @@ q = query(x) # (B, T, 16)
 wei =  q @ k.transpose(-2, -1) # (B, T, 16) @ (B, 16, T) ---> (B, T, T)
 
 tril = torch.tril(torch.ones(T, T))
-#wei = torch.zeros((T,T))
+wei = torch.zeros((T,T))
 wei = wei.masked_fill(tril == 0, float('-inf'))
 wei = F.softmax(wei, dim=-1)
 
