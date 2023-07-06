b35f41095aa2521ca1786d24fe340ce2ad9b1e9b
Jaward Sesay <jkef96@icloud.com>
2023-07-06 18:59:50 +0800

    Delete inference_with_pipeline.py

--- HuggingFace-Transformer/inference_with_pipeline.py
+++
@@ -1,5 +0,0 @@
-from transformers import pipeline
-
-generator = pipeline(task="openai/whisper-large")
-generator("https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/mlk.flac")
-{'text': ' I have a dream that one day this nation will rise up and live out the true meaning of its creed.'}
