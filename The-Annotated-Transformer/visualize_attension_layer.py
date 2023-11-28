4ce67c0b45f6919a32e433b549505d5599d16b0a
Jaward Sesay <jkef96@icloud.com>
2023-11-28 19:33:38 +0800

    visualize self-attension layer

--- The-Annotated-Transformer/visualize_attension_layer.py
+++ The-Annotated-Transformer/visualize_attension_layer.py
@@ -83,3 +83,28 @@ def visualize_layer(model, layer, getter_fn, ntokens, row_tokens, col_tokens):
         # | charts[7]
         # layer + 1 due to 0-indexing
     ).properties(title="Layer %d" % (layer + 1))
+
+
+def viz_encoder_self():
+    model, example_data = run_model_example(n_examples=1)
+    example = example_data[
+        len(example_data) - 1
+    ]  # batch object for the final example
+
+    layer_viz = [
+        visualize_layer(
+            model, layer, get_encoder, len(example[1]), example[1], example[1]
+        )
+        for layer in range(6)
+    ]
+    return alt.hconcat(
+        layer_viz[0]
+        # & layer_viz[1]
+        & layer_viz[2]
+        # & layer_viz[3]
+        & layer_viz[4]
+        # & layer_viz[5]
+    )
+
+
+show_example(viz_encoder_self)
