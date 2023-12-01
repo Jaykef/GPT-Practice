8993a3aafd93cdcf14bfca47648fec609d59afb6
Jaward Sesay <jkef96@icloud.com>
2023-12-01 13:47:28 +0800

    visualize decoder

--- The-Annotated-Transformer/visualize_attension_layer.py
+++ The-Annotated-Transformer/visualize_attension_layer.py
@@ -135,3 +135,31 @@ def viz_decoder_self():
 
 
 show_example(viz_decoder_self)
+
+
+def viz_decoder_src():
+    model, example_data = run_model_example(n_examples=1)
+    example = example_data[len(example_data) - 1]
+
+    layer_viz = [
+        visualize_layer(
+            model,
+            layer,
+            get_decoder_src,
+            max(len(example[1]), len(example[2])),
+            example[1],
+            example[2],
+        )
+        for layer in range(6)
+    ]
+    return alt.hconcat(
+        layer_viz[0]
+        & layer_viz[1]
+        & layer_viz[2]
+        & layer_viz[3]
+        & layer_viz[4]
+        & layer_viz[5]
+    )
+
+
+show_example(viz_decoder_src)
